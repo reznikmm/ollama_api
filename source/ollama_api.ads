efa0a9ebea413ba0e95814bce9295877bb7f4307
Maxim Reznik <reznikmm@gmail.com>
2026-02-10 10:29:03 +0200

    Import initial stub files

---
+++ source/ollama_api.ads
@@ -0,0 +1,8 @@
+--  SPDX-FileCopyrightText: 2026 Max Reznik <reznikmm@gmail.com>
+--
+--  SPDX-License-Identifier: MIT OR Apache-2.0 WITH LLVM-exception
+------------------------------------------------------------------
+
+package Ollama_API is
+
+end Ollama_API;
